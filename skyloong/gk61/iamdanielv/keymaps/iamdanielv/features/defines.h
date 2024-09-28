b2d90445b56b764cb9b25ba96d7ff9206b84074a
Daniel <1767914+iamdanielv@users.noreply.github.com>
2024-09-28 07:00:20 -0700

    move defines
    keymap is getting bigger, break up the code

---
+++ skyloong/gk61/iamdanielv/keymaps/iamdanielv/features/defines.h
@@ -0,0 +1,39 @@
+#pragma once
+
+enum layer_names {
+    _WIN_LYR,     // 0
+    _WIN_FN_LYR,  // 1
+    _CTL_LYR,     // 2
+    _NUM_LYR,     // 3
+    _NAV_LYR,     // 4
+    _FN_LYR,      // 5
+};
+
+// ******************************
+// * Aliases to simplify keymap *
+// ******************************
+#define FN_W_CAPS LT(_WIN_FN_LYR, KC_CAPS)
+#define FN_W_RALT LT(_WIN_FN_LYR, KC_RALT)
+
+#define MY_UNDO   C(KC_Z)
+#define MY_CUT    C(KC_X)
+#define MY_COPY   C(KC_INS)
+#define MY_PASTE  S(KC_INS)
+#define MY_TASK   LCTL(LSFT(KC_ESC))
+#define MY_CONS   LCTL(LSFT(KC_GRV))
+#define MY_BACK   A(KC_LEFT)
+#define MY_FWD    A(KC_RIGHT)
+#define MY_GRV    LT(0, KC_GRV)
+
+#define FN_LEFT   LT(_FN_LYR, KC_LEFT)
+#define APP_DWN   TD(_DN_MU)
+#define RCTL_RGT  RCTL_T(KC_RIGHT)
+#define RSFT_UP   RSFT_T(KC_UP)
+
+#define TG_NAV    TG(_NAV_LYR)
+#define TG_NUM    TG(_NUM_LYR)
+#define TG_CTL    TG(_CTL_LYR)
+#define MO_CTL    MO(_CTL_LYR)
+#define TD_TG_CTL TD(TD_CTL_TG)
+#define TD_KB_RST TD(TD_RESET)
+#define TD_KB_CLR TD(TD_CLEAR)
--- skyloong/gk61/iamdanielv/keymaps/iamdanielv/keymap.c
+++ skyloong/gk61/iamdanielv/keymaps/iamdanielv/keymap.c
@@ -7,17 +7,9 @@
 #include "color.h"
 #include "quantum.h"
 
+#include "features/defines.h"
 #include "features/indicator_queue.h"
 #include "features/fn_mode.h"
-
-enum layer_names {
-    _WIN_LYR,     // 0
-    _WIN_FN_LYR,  // 1
-    _CTL_LYR,     // 2
-    _NUM_LYR,     // 3
-    _NAV_LYR,     // 4
-    _FN_LYR,      // 5
-};
 
 void blink_arrows(void);
 void blink_NKRO(bool);
@@ -295,36 +287,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
-
-
-// ******************************
-// * Aliases to simplify keymap *
-// ******************************
-#define FN_W_CAPS LT(_WIN_FN_LYR, KC_CAPS)
-#define FN_W_RALT LT(_WIN_FN_LYR, KC_RALT)
-
-#define MY_UNDO   C(KC_Z)
-#define MY_CUT    C(KC_X)
-#define MY_COPY   C(KC_INS)
-#define MY_PASTE  S(KC_INS)
-#define MY_TASK   LCTL(LSFT(KC_ESC))
-#define MY_CONS   LCTL(LSFT(KC_GRV))
-#define MY_BACK   A(KC_LEFT)
-#define MY_FWD    A(KC_RIGHT)
-
-#define FN_LEFT   LT(_FN_LYR, KC_LEFT)
-#define APP_DWN   TD(_DN_MU)
-#define RCTL_RGT  RCTL_T(KC_RIGHT)
-#define RSFT_UP   RSFT_T(KC_UP)
-
-#define TG_NAV    TG(_NAV_LYR)
-#define TG_NUM    TG(_NUM_LYR)
-#define TG_CTL    TG(_CTL_LYR)
-#define MO_CTL    MO(_CTL_LYR)
-#define TD_TG_CTL TD(TD_CTL_TG)
-#define TD_KB_RST TD(TD_RESET)
-#define TD_KB_CLR TD(TD_CLEAR)
-
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     // to use this, make sure to put:
