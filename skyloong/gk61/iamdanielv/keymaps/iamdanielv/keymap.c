610fa7eb8374db53c97758f5e6b069552648cb7a
Daniel <1767914+iamdanielv@users.noreply.github.com>
2025-03-21 18:15:20 -0700

    Change EXT Layer to act more like FN Layer
    Instead of treating EXT as a standalone layer, use it more like a Win FN Layer.
    
    EXT Layer now has FN keys on the numbers. It no longer allows toggling of the number keys to FN keys, that is now done only from the kbd ctl layer.
    
    The Base layer now uses MY_GRV for esc and grave, the EXT Layer now replaces the ESC key with the GRV key to allow easy access without a tap dance.

--- skyloong/gk61/iamdanielv/keymaps/iamdanielv/keymap.c
+++ skyloong/gk61/iamdanielv/keymaps/iamdanielv/keymap.c
@@ -61,25 +61,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
     [BASE_LYR] = LAYOUT_all(
-       KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+       MY_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
        KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
        MO_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,  KC_QUOT,            MY_ENT,
        KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,            UP_RSFT,
        KC_LCTL,   KC_LGUI,   KC_LALT,              KC_SPC,    KC_SPC,    KC_MUTE,              KC_SPC,    ARWS_RALT, KBCTL_LFT,DN_APP,             RCTL_RGT
     ),
     [HRM_BASE_LYR] =  LAYOUT_all(
-       MY_GRV,    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______, _______,  _______,
+       _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______, _______,  _______,
        _______,   _______,   _______,   _______,   CTLR_R,    CTLH_T,    _______,   _______,   _______,   _______,   _______,   _______, _______,  _______,
        _______,   GUI_A,     ALT_S,     SFT_D,     CTL_F,     CTLG_G,    _______,   CTL_J,     SFT_K,     ALT_L,     HM_SCLN,   END_QUOT,          _______,
        LSFT_LLCK, _______,   _______,   CTLS_C,    _______,   _______,   _______,   _______,   ALFT_COMM, ARGT_DOT,  _______,            _______,
        _______,   _______,   _______,              _______,   _______,   _______,              _______,   _______,   _______,   _______,           _______
     ),
     [EXT_LYR] = LAYOUT_all(
-       KC_GRV,    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,  _______,
+       KC_GRV,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,   KC_F11,   KC_F12,   KC_DEL,
        _______,   MY_CONS,   MY_TASK,   C(KC_F),   C(KC_R),   C(KC_H),   KC_PGUP,   KC_HOME,   KC_UP,     KC_END,    KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
        _______,   KC_LGUI,   KC_LALT,   KC_LSFT,   KC_LCTL,   C(KC_G),   KC_PGDN,   KC_LEFT,   KC_DOWN,   KC_RIGHT,  KC_HOME,  KC_END,             _______,
-       LSFT_LLCK, MY_UNDO,   MY_CUT,    MY_COPY,   MY_PASTE,  KC_SPC,    KC_BSPC,   KC_DEL,    MY_BACK,   MY_FWD,    _______,            _______,
-       KC_SWP_FN, _______,   _______,              _______,   KBCTL_SPC, _______,              _______,   _______,   _______,  _______,            _______
+       LSFT_LLCK, MY_UNDO,   MY_CUT,    MY_COPY,   MY_PASTE,  KC_SPC,    KC_BSPC,   KC_DEL,    MY_BACK,   MY_FWD,    _______,            KC_VOLU,
+       _______,   _______,   _______,              _______,   KBCTL_SPC, _______,              _______,   _______,   MY_BACK,  KC_VOLD,            MY_FWD
     ),
     [KBCTL_LYR] = LAYOUT_all(
        _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
