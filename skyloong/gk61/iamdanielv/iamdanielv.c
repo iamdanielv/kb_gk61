940596443069a97f89ae00832cc722f8b9b359e2
Daniel <1767914+iamdanielv@users.noreply.github.com>
2025-04-19 05:21:50 -0700

    Change Caplock to Red
    Makes it stand out more

--- skyloong/gk61/iamdanielv/iamdanielv.c
+++ skyloong/gk61/iamdanielv/iamdanielv.c
@@ -86,7 +86,7 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     }
 
     if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 255, 0, 0);
     } else {
         if (!rgb_matrix_get_flags()) {
             RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_INDEX, 0, 0, 0);
